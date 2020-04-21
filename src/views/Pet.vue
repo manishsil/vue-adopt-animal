a1d561aab8f1534daaf3cfafe01e61861164a5c3
Manish Sil <manishsil@yahoo.com>
2020-04-21 22:40:54 +0530

    initial commit for animal adoptation app

---
+++ src/views/Pet.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>{{displayAnimal.name}}({{$route.params.species}})</h1>
+        <p>Breed : {{displayAnimal.breed}}</p>
+        <p>Genfer: {{displayAnimal.gender}}</p>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  data () {
+    return {
+      displayAnimal: {}
+    }
+  },
+  computed: {
+    ...mapState(['dogs', 'cats'])
+  },
+  mounted () {
+    const animal = this[this.$route.params.species][this.$route.params.id]
+    this.displayAnimal = animal
+  }
+}
+</script>
